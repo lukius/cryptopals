fceb61ef6fc7e15c50e1f6eb7a624cdcc87a6a1e
Lucio Santi <lsanti@dc.uba.ar>
2016-11-10 17:30:24 -0300

    Starting S8C57.

---
+++ set8/challenge57.py
@@ -0,0 +1,10 @@
+from common.challenge import MatasanoChallenge
+
+
+class Set8Challenge57(MatasanoChallenge):
+    
+    def expected_value(self):
+        return
+    
+    def value(self):
+        return    
