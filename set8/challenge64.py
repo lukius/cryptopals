41e4dd5007e83309b0f24e1b1281c3a12ed1f31e
Lucio Santi <lukius@gmail.com>
2016-12-11 23:21:29 -0300

    Improved challenge 64.

--- set8/challenge64.py
+++ set8/challenge64.py
@@ -7,6 +7,48 @@ from common.math.linalg.bit import BitMatrix
 from common.tools.misc import RandomByteGenerator
 
 
+class Challenge64GCM(GCM):
+    
+    # Custom GCM implementation for this challenge. Precomputes the portion of
+    # the GHASH that is not affected by the differences introduced into the
+    # ciphertex by the truncated MAC attack. This speeds up the vector
+    # discovery phase after finding the kernel of the dependency matrix.
+    
+    def __init__(self, iv, tag_length=None):
+        GCM.__init__(self, iv, tag_length=tag_length)
+        self.hash = None
+        
+    def _is_power_of_2(self, i):
+        return i & (i-1) == 0
+        
+    def _precompute_(self, ciphertext):
+        self.hash = 0
+        n = ciphertext.block_count() + 1
+        h = self._to_field_elem(self.H)
+        for block in ciphertext:
+            if not self._is_power_of_2(n):
+                z = self._to_field_elem(block)
+                self.hash += z * h**n
+            n -= 1
+        
+    def _ghash(self, cipher, auth_data, ciphertext):
+        if self.hash is None:
+            self._precompute_(ciphertext)
+        n = ciphertext.block_count()
+        j = 2
+        h = self._to_field_elem(self.H)
+        ghash = self.hash
+        while j <= n:
+            block = ciphertext.get_block(n - j + 1)
+            z = self._to_field_elem(block)
+            ghash += z * h**j
+            j <<= 1
+        len_C = len(ciphertext)*8
+        L = self.field.element(len_C)         
+        return ghash + L*h  
+        
+        
+
 class Set8Challenge64(CryptoChallenge):
     
     def _test_GF2k_linear_operations(self):
@@ -47,7 +89,7 @@ class Set8Challenge64(CryptoChallenge):
         
     def _test_key_recovery(self):
         byte_generator = RandomByteGenerator()
-        gcm_iv = byte_generator.value(16)
+        gcm_iv = byte_generator.value(12)
         aes_key = byte_generator.value(16)
         # NOTE: the 32-bit tag attack works but takes a considerable amount of
         # time to complete, even with concurrency optimizations. This 16-bit
@@ -57,7 +99,7 @@ class Set8Challenge64(CryptoChallenge):
         message = byte_generator.value(16 * 2**9)
 
         aes = AES(aes_key)
-        gcm = GCM(gcm_iv, tag_length=16)
+        gcm = Challenge64GCM(gcm_iv, tag_length=16)
         c, t = aes.encrypt(message, mode=gcm)
         
         attack = TruncatedMACGCMAttack(aes, gcm, c, t)
